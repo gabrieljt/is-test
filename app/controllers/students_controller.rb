69c26d0eb5a59208dc48881811ec8450e0e4ede4
Gabriel Trabasso <gabrieljt@gmail.com>
2014-09-23 17:14:43 -0300

    Student simple CRUD

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,52 @@
 class StudentsController < ApplicationController
+	def index
+		@students = Student.all
+	end
+
+	def show
+		@student = Student.find(params[:id])
+	end
+
+	def new
+		@student = Student.new
+		@statuses = Student.statuses
+	end
+
+	def create
+		@student = Student.new(student_params)
+		@statuses = Student.statuses
+		if @student.save
+			redirect_to @student
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@student = Student.find(params[:id])
+		@statuses = Student.statuses
+	end
+
+	def update
+		@student = Student.find(params[:id])
+		@statuses = Student.statuses
+
+		if @student.update(student_params)
+			redirect_to @student
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@student = Student.find(params[:id])
+		@student.destroy
+
+		redirect_to students_path
+	end
+
+	private 
+		def student_params
+			params.require(:student).permit(:name, :register_number, :status)
+		end	
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
 class Student < ActiveRecord::Base
+	enum status: [ :idle, :enrolled ]
+	validates :name, presence: true
+	validates :register_number, presence: true, uniqueness: true
+	validates :status, presence: true
 end
