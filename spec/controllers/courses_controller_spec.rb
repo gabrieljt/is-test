3bca331ca07bdd4ec120331febefda993b968723
Gabriel Trabasso <gabrieljt@gmail.com>
2014-10-07 04:54:06 -0300

    Controller get index tests

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe CoursesController, :type => :controller do
+RSpec.describe CoursesController, :type => :controller do	
+	describe "GET index" do		
+	    it "assigns @courses" do
+	    	courses = Course.all
+	      	get :index
+	      	expect(assigns(:courses)).to eq(courses)
+	    end
 
+	    it "renders the index template" do
+	    	get :index
+	      	expect(response).to render_template("index")
+	    end
+  	end
 end
--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe StudentsController, :type => :controller do
+	describe "GET index" do		
+	    it "assigns @students" do
+	    	students = Student.all
+	      	get :index
+	      	expect(assigns(:students)).to eq(students)
+	    end
 
+	    it "renders the index template" do
+	    	get :index
+	      	expect(response).to render_template("index")
+	    end
+  	end
 end
