1ffd311e78da8234fdccac8c63b78ac623f94b86
Gabriel Trabasso <gabrieljt@gmail.com>
2014-10-07 06:24:02 -0300

    Controllers tests

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,44 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe CoursesController, :type => :controller do	
-	describe "GET index" do		
-	    it "assigns @courses" do
+	describe 'GET index' do		
+	    it 'assigns @courses' do
 	    	courses = Course.all
 	      	get :index
 	      	expect(assigns(:courses)).to eq(courses)
 	    end
 
-	    it "renders the index template" do
+	    it 'renders the index template' do
 	    	get :index
-	      	expect(response).to render_template("index")
+	      	expect(response).to render_template('index')
 	    end
   	end
 
-  	describe "GET show" do		
-	    it "assigns @course" do
+  	describe 'GET show' do		
+	    it 'assigns @course' do
 	    	course = Course.first
 	      	get :show, id: course
 	      	expect(assigns(:course)).to eq(course)
 	    end
 
-	    it "renders the show template" do
+	    it 'renders the show template' do
 	    	get :show, id: Course.first
-	      	expect(response).to render_template("show")
+	      	expect(response).to render_template('show')
 	    end
   	end
 
-  	describe "GET new" do		
-	    it "assigns @course, @statuses" do
-	    	course = Course.new
+  	describe 'GET new' do		
+	    it 'assigns @course, @statuses' do
 	    	statuses = Course.statuses
+	    	course = Course.new	    	
 	      	get :new
 	      	expect(assigns(:course).attributes).to eq(course.attributes)
 	      	expect(assigns(:statuses)).to eq(statuses)
 	    end
 
-	    it "renders the new template" do
+	    it 'renders the new template' do
 	    	get :new
-	      	expect(response).to render_template("new")
+	      	expect(response).to render_template('new')
 	    end
-  	end
+  	end  	
 end
--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,42 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe StudentsController, :type => :controller do
-	describe "GET index" do		
-	    it "assigns @students" do
+	describe 'GET index' do		
+	    it 'assigns @students' do
 	    	students = Student.all
 	      	get :index
 	      	expect(assigns(:students)).to eq(students)
 	    end
 
-	    it "renders the index template" do
+	    it 'renders the index template' do
 	    	get :index
-	      	expect(response).to render_template("index")
+	      	expect(response).to render_template('index')
 	    end
   	end
 
-  	describe "GET show" do		
-	    it "assigns @student" do
+  	describe 'GET show' do		
+	    it 'assigns @student' do
 	    	student = Student.first
 	      	get :show, id: student
 	      	expect(assigns(:student)).to eq(student)
 	    end
 
-	    it "renders the show template" do
+	    it 'renders the show template' do
 	    	get :show, id: Student.first
-	      	expect(response).to render_template("show")
+	      	expect(response).to render_template('show')
 	    end
   	end
 
-  	describe "GET new" do		
-	    it "assigns @student" do
+  	describe 'GET new' do		
+	    it 'assigns @student' do
 	    	student = Student.new
 	      	get :new
 	      	expect(assigns(:student).attributes).to eq(student.attributes)
 	    end
 
-	    it "renders the new template" do
+	    it 'renders the new template' do
 	    	get :new
-	      	expect(response).to render_template("new")
+	      	expect(response).to render_template('new')
 	    end
   	end
 end
