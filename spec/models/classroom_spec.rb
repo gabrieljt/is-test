e353de0014c8b69bf140946271504d8fd9fe542d
Gabriel Trabasso <gabrieljt@gmail.com>
2014-10-07 04:05:29 -0300

    Classroom unit test

--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Classroom, :type => :model do
 	student_name = 'John Doe'
 	Course.create(name: course_name, description: 'Bar', status: 0)
 	Student.create(name: student_name)
+	Classroom.create(course_id: 1, student_id: 1)
 
 	describe '#new' do
 		context 'when default constructor' do
@@ -31,13 +32,39 @@ RSpec.describe Classroom, :type => :model do
   			end
 		end
 
-		context 'when Course adds Student' do
-			course = Course.find_by(name: course_name)			
-			student = Student.find_by(name: student_name)
-			course.students.create(student.attributes)
+		context 'when :course, :student' do
+			course = Course.new(name: 'ORM', description: 'Learn how to associate models like a boss.', status: 0)
+			student = Student.new(name: 'Foo')
+			classroom = Classroom.new(course: course, student: student)
+			
+			it 'is valid' do
+  				expect(classroom.valid?).to be_truthy  				
+  			end
 
-			it 'is saved' do
-				expect(course.students).not_to be_empty
+  			it 'has 0 errors' do
+  				expect(classroom.errors.count).to be_zero
+  			end
+
+  			it 'can be saved' do
+  				expect(classroom.save).to be_truthy
+  			end
+		end
+
+		context 'when :student is duplicated in :course' do
+			course = Course.find(1)			
+			student = Student.find(1)
+			classroom = Classroom.new(course: course, student: student)
+
+			it 'is not valid' do
+				expect(classroom.valid?).to be_falsey
+			end
+
+			it 'has :student error' do
+				expect(classroom.errors).to have_key(:student_id)
+			end
+
+			it 'cannot be saved' do
+				expect(classroom.save).to be_falsey
 			end
 		end
 	end
